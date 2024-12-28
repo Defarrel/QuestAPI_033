23113921b738140e00419150c2497517abbafd24
defarrel <Defarrel>
2024-12-28 17:47:11 +0700

    Menambahkan EntryMhsScreen dan DestinasiEntry

--- app/src/main/java/com/example/tugas_pertemuan12/ui/view/InsertView.kt
+++ app/src/main/java/com/example/tugas_pertemuan12/ui/view/InsertView.kt
@@ -4,16 +4,64 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tugas_pertemuan12.navigation.DestinasiNavigasi
+import com.example.tugas_pertemuan12.ui.customwidget.CostumeTopAppBar
 import com.example.tugas_pertemuan12.ui.viewmodel.InsertUiEvent
 import com.example.tugas_pertemuan12.ui.viewmodel.InsertUiState
+import com.example.tugas_pertemuan12.ui.viewmodel.InsertViewModel
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehaviour.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehaviour,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveMhs()
+                } },
+            modifier = Modifier.padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntryBody(
