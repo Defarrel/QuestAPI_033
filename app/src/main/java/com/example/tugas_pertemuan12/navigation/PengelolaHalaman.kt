df434712bafbb47ebbfccaf7c2c443e525308015
defarrel <Defarrel>
2024-12-28 19:23:11 +0700

    Menambah PengelolaHalaman

---
+++ app/src/main/java/com/example/tugas_pertemuan12/navigation/PengelolaHalaman.kt
@@ -0,0 +1,41 @@
+package com.example.tugas_pertemuan12.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tugas_pertemuan12.ui.view.DestinasiEntry
+import com.example.tugas_pertemuan12.ui.view.DestinasiHome
+import com.example.tugas_pertemuan12.ui.view.EntryMhsScreen
+import com.example.tugas_pertemuan12.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(route = DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
