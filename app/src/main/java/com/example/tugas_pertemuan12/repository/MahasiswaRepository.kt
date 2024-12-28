15090eed7f20809367ecb08f407613d0508edb08
defarrel <Defarrel>
2024-12-28 16:20:30 +0700

    Membuat project quest api

---
+++ app/src/main/java/com/example/tugas_pertemuan12/repository/MahasiswaRepository.kt
@@ -0,0 +1,48 @@
+package com.example.tugas_pertemuan12.repository
+
+import com.example.tugas_pertemuan12.model.Mahasiswa
+import java.io.IOException
+
+interface MahasiswaRepository{
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun getMahasiswaByNim(nim: String): Mahasiswa
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
+}
+
+class NetworkKontakRepository(
+    private val kontakApiService: MahasiswaRepository
+): MahasiswaRepository{
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        kontakApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> {
+        return kontakApiService.getMahasiswa()
+    }
+
+    override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
+        return kontakApiService.getMahasiswaByNim(nim)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        kontakApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = kontakApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete kontak. HTTP Status code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+}
