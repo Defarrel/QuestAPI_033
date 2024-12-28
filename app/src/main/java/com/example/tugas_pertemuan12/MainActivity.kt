165ccc9508436258102da3624dc275c5b46162bc
defarrel <Defarrel>
2024-12-28 19:39:35 +0700

    Menambahkan MahasiswaApp

--- app/src/main/java/com/example/tugas_pertemuan12/MainActivity.kt
+++ app/src/main/java/com/example/tugas_pertemuan12/MainActivity.kt
@@ -5,13 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.tugas_pertemuan12.ui.theme.Tugas_Pertemuan12Theme
+import com.example.tugas_pertemuan12.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +17,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tugas_Pertemuan12Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MahasiswaApp(
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Tugas_Pertemuan12Theme {
-        Greeting("Android")
     }
 }
